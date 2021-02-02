618e12c9a22c2998aab9e8410ca097935219588a
Prakhar Jaiman <jaimanprakhar12345@gmail.com>
2021-02-02 15:55:28 +0530

    skeleteon page where to display

---
+++ src/collections/skeleton/GradientStop.vue
@@ -0,0 +1,22 @@
+<template>
+  <stop
+        :offset="offset"
+        :stop-color="stopColor"
+        :stop-opacity="stopOpacity"
+    >
+        <animate
+            attributeName="offset"
+            :values="values"
+            :dur="dur"
+            repeatCount="indefinite"
+            v-if="animate"
+        />
+    </stop>
+</template>
+
+<script>
+export default {
+    props:['offset', 'stopColor', 'stopOpacity', 'dur', 'animate', 'values']
+
+}
+</script>
