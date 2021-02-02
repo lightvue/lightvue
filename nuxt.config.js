c6c18c006e767bd208f611e5ff73456342f2c4fa
Ishaan Mehta <ishaanmehta4@gmail.com>
2021-02-02 22:53:50 +0530

    Added search functionality

--- nuxt.config.js
+++ nuxt.config.js
@@ -62,5 +62,7 @@ export default {
   // },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {},
+  build: {
+    transpile: ['vue-instantsearch', 'instantsearch.js/es']
+  },
 };
