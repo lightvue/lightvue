55fb9c73b62b27b845b6eda87039b98f1c818b41
Prakhar Jaiman <jaimanprakhar12345@gmail.com>
2021-02-02 15:41:58 +0530

    progress-loader

---
+++ src/examples/progressloader/ProgressLoaderDemo_ball.vue
@@ -0,0 +1,151 @@
+<template>
+  <div class="loader-demo-wrapper">
+    <div class="card">
+        <h3>TYPE: <strong>ball-beat</strong></h3>
+        <span class="demo__wrapper">
+        <div class="demo__item"><lv-loader type="ball-beat"/><span>Default</span></div>
+        <div class="demo__item"><lv-loader type="ball-beat" color="#38b2ac"/><span>With color prop</span></div>
+        <div class="demo__item"><lv-loader type="ball-beat" :scale="2"/><span>With scale prop</span></div>
+      </span>
+    </div>
+    <div class="card">
+        <h3>TYPE: <strong>ball-pulse</strong></h3>
+        <span class="demo__wrapper">
+        <div class="demo__item"><lv-loader type="ball-pulse"/><span>Default</span></div>
+        <div class="demo__item"><lv-loader type="ball-pulse" color="#38b2ac"/><span>With color prop</span></div>
+        <div class="demo__item"><lv-loader type="ball-pulse" :scale="2"/><span>With scale prop</span></div>
+      </span>
+    </div>
+    <div class="card">
+        <h3>TYPE: <strong>ball-scale</strong></h3>
+        <span class="demo__wrapper">
+        <div class="demo__item"><lv-loader type="ball-scale"/><span>Default</span></div>
+        <div class="demo__item"><lv-loader type="ball-scale" color="#38b2ac"/><span>With color prop</span></div>
+        <div class="demo__item"><lv-loader type="ball-scale" :scale="2"/><span>With scale prop</span></div>
+      </span>
+    </div>
+    <div class="card">
+        <h3>TYPE: <strong>ball-pulse-rise</strong></h3>
+        <span class="demo__wrapper">
+        <div class="demo__item"><lv-loader type="ball-pulse-rise"/><span>Default</span></div>
+        <div class="demo__item"><lv-loader type="ball-pulse-rise" color="#38b2ac"/><span>With color prop</span></div>
+        <div class="demo__item"><lv-loader type="ball-pulse-rise" :scale="2"/><span>With scale prop</span></div>
+      </span>
+    </div>
+    <div class="card">
+        <h3>TYPE: <strong>ball-grid-pulse</strong></h3>
+        <span class="demo__wrapper">
+        <div class="demo__item"><lv-loader type="ball-grid-pulse"/><span>Default</span></div>
+        <div class="demo__item"><lv-loader type="ball-grid-pulse" color="#38b2ac"/><span>With color prop</span></div>
+        <div class="demo__item"><lv-loader type="ball-grid-pulse" :scale="2"/><span>With scale prop</span></div>
+      </span>
+    </div>
+    <div class="card">
+        <h3>TYPE: <strong>ball-clip-rotate</strong></h3>
+        <span class="demo__wrapper">
+        <div class="demo__item"><lv-loader type="ball-clip-rotate"/><span>Default</span></div>
+        <div class="demo__item"><lv-loader type="ball-clip-rotate" color="#38b2ac"/><span>With color prop</span></div>
+        <div class="demo__item"><lv-loader type="ball-clip-rotate" :scale="2"/><span>With scale prop</span></div>
+      </span>
+    </div>
+    <div class="card">
+        <h3>TYPE: <strong>ball-clip-rotate-multiple</strong></h3>
+        <span class="demo__wrapper">
+        <div class="demo__item"><lv-loader type="ball-clip-rotate-multiple"/><span>Default</span></div>
+        <div class="demo__item"><lv-loader type="ball-clip-rotate-multiple" color="#38b2ac"/><span>With color prop</span></div>
+        <div class="demo__item"><lv-loader type="ball-clip-rotate-multiple" :scale="2"/><span>With scale prop</span></div>
+      </span>
+    </div>
+    <div class="card">
+        <h3>TYPE: <strong>ball-clip-rotate-pulse</strong></h3>
+        <span class="demo__wrapper">
+        <div class="demo__item"><lv-loader type="ball-clip-rotate-pulse"/><span>Default</span></div>
+        <div class="demo__item"><lv-loader type="ball-clip-rotate-pulse" color="#38b2ac"/><span>With color prop</span></div>
+        <div class="demo__item"><lv-loader type="ball-clip-rotate-pulse" :scale="2"/><span>With scale prop</span></div>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped lang="scss">
+* {
+  box-sizing: border-box;
+}
+
+.loader-demo-wrapper {
+  display: flex;
+  height: max-content;
+  flex-flow: row wrap;
+}
+.card {
+    width: calc(50% - 10px);
+    min-width: 200px;
+    border-radius: 4px;
+    padding: 20px;
+    padding-bottom: 0px;
+    margin: 5px;
+}
+
+.card h3 {
+    color: #607c8a;
+    margin-bottom: 40px;
+    font-size: 1.4em;
+    font-weight: normal;
+
+}
+
+.demo__wrapper {
+  display: flex;
+  flex-flow: row wrap;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin: 0px;
+  padding: 0px;
+  height: max-content;
+  border: 0px;
+}
+
+.demo__item {
+  display: inline-block;
+  flex-flow: row wrap;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  border: 0px;
+  height: 140px;
+  width: 33%;
+  margin: 0px;
+  padding: 0px;
+  min-width: 160px;
+}
+
+.demo__item span {
+    display: inline-block;
+    position: relative;
+    top: 20px;
+    background-color: #607c8a;
+    width: max-content;
+    width: 100%;
+    font-size: 14px;
+    text-align: center;
+    padding: 5px;
+    margin: 0;
+    color: white;
+    opacity: 0;
+    transition: all 0.1s linear;
+}
+
+.demo__item:hover span {
+    opacity: 1;
+    top: 0px;
+}
+
+</style>
---
+++ src/pages/vue-components/progress-loader.vue
@@ -0,0 +1,65 @@
+<template>
+  <docs-page-layout :title="$options.title" :description="$options.description">
+    <docs-card-vue title="TYPE: ball-scale" file="examples/progressloader/BallTypeDemos/BallScaleDemo" />
+    <docs-card-vue title="TYPE: ball-scale-multiple" file="examples/progressloader/BallTypeDemos/BallScaleMultipleDemo" />
+    <docs-card-vue title="TYPE: ball-scale-random" file="examples/progressloader/BallTypeDemos/BallScaleRandomDemo" />
+    <docs-card-vue title="TYPE: ball-scale-ripple" file="examples/progressloader/BallTypeDemos/BallScaleRippleDemo" />
+    <docs-card-vue title="TYPE: ball-scale-ripple-multiple" file="examples/progressloader/BallTypeDemos/BallScaleRippleMultipleDemo" />
+    <docs-card-vue title="TYPE: ball-beat" file="examples/progressloader/BallTypeDemos/BallBeatDemo" />
+    <docs-card-vue title="TYPE: ball-pulse" file="examples/progressloader/BallTypeDemos/BallPulseDemo" />
+    <docs-card-vue title="TYPE: ball-pulse-sync" file="examples/progressloader/BallTypeDemos/BallPulseSyncDemo" />
+    <docs-card-vue title="TYPE: ball-pulse-rise" file="examples/progressloader/BallTypeDemos/BallPulseRiseDemo" />
+    <docs-card-vue title="TYPE: ball-rotate" file="examples/progressloader/BallTypeDemos/BallRotateDemo" />
+    <docs-card-vue title="TYPE: ball-grid-beat" file="examples/progressloader/BallTypeDemos/BallGridBeatDemo" />
+    <docs-card-vue title="TYPE: ball-grid-pulse" file="examples/progressloader/BallTypeDemos/BallGridPulseDemo" />
+    <docs-card-vue title="TYPE: ball-spin-fade-loader" file="examples/progressloader/BallTypeDemos/BallSpinFadeLoaderDemo" />
+    <docs-card-vue title="TYPE: ball-zig-zag" file="examples/progressloader/BallTypeDemos/BallZigZagDemo" />
+    <docs-card-vue title="TYPE: ball-zig-zag-deflect" file="examples/progressloader/BallTypeDemos/BallZigZagDeflectDemo" />
+    <docs-card-vue title="TYPE: ball-triangle-path" file="examples/progressloader/BallTypeDemos/BallTrianglePathDemo" />
+    <docs-card-vue title="TYPE: ball-clip-rotate" file="examples/progressloader/BallTypeDemos/BallClipRotateDemo" />
+    <docs-card-vue title="TYPE: ball-clip-rotate-multiple" file="examples/progressloader/BallTypeDemos/BallClipRotateMultipleDemo" />
+    <docs-card-vue title="TYPE: line-scale" file="examples/progressloader/LineTypeDemos/LineScaleDemo" />
+    <docs-card-vue title="TYPE: line-scale-party" file="examples/progressloader/LineTypeDemos/LineScalePartyDemo" />
+    <docs-card-vue title="TYPE: line-scale-pulse-out" file="examples/progressloader/LineTypeDemos/LineScalePulseOutDemo" />
+    <docs-card-vue title="TYPE: line-scale-pusle-out-rapid" file="examples/progressloader/LineTypeDemos/LineScalePulseOutRapidDemo" />
+    <docs-card-vue title="TYPE: line-spin-fade-loader" file="examples/progressloader/LineTypeDemos/LineSpinFadeLoaderDemo" />
+    <docs-card-vue title="TYPE: square-spin" file="examples/progressloader/MiscTypeDemos/SquareSpinDemo" />
+    <docs-card-vue title="TYPE: cube-transition" file="examples/progressloader/MiscTypeDemos/CubeTransitionDemo" />
+    <docs-card-vue title="TYPE: semi-circle-spin" file="examples/progressloader/MiscTypeDemos/SemiCircleSpinDemo" />
+    <!-- Needs color fix -->
+    <docs-card-vue title="TYPE: pacman" file="examples/progressloader/MiscTypeDemos/PacmanDemo" />
+    <!-- Needs color fix -->
+    <docs-card-vue title="TYPE: triangle-skew-spin" file="examples/progressloader/MiscTypeDemos/TriangleSkewSpinDemo" />
+    <!-- Needs color fix -->
+    <template #api>
+      <getting-started :package-name="$options.packageName" :component-name="$options.componentName" />
+      <docs-card title="APIs" :responsive="false">
+        <docs-all-api :api-data="$options.apiData" />
+      </docs-card>
+    </template>
+  </docs-page-layout>
+</template>
+
+<script>
+import DocsPageLayout from '@/components/docs-card/DocsPageLayout.vue';
+import DocsCardVue from '@/components/docs-card/DocsCardVue.vue';
+import DocsCard from '@/components/docs-card/DocsCard.vue';
+import GettingStarted from '@/components/docs-card/GettingStarted.vue';
+import DocsAllApi from '@/components/docs-card/DocsAllApi.vue';
+import ProgressLoaderAPI from '@/collections/progressloader/ProgressLoaderAPI';
+
+export default {
+  components: {
+    DocsCardVue,
+    DocsCard,
+    GettingStarted,
+    DocsPageLayout,
+    DocsAllApi,
+  },
+  title: 'Progress Loaders',
+  description: `Progress Loader components can be used as a loading animations or space-fillers while waiting for the actual content to load.`,
+  packageName: 'progressloader',
+  componentName: 'LvLoader',
+  apiData: ProgressLoaderAPI,
+};
+</script>
