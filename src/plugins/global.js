959e8bdc9a8a8e4fea196792296438604a72e5c4
aankushh <ankushkhandelwal67@gmail.com>
2021-02-11 03:59:21 +0530

    Nuxt -- gta

--- src/plugins/global.js
+++ src/plugins/global.js
@@ -1,6 +1,12 @@
 import Vue from 'vue';
 import LvInput from '@/collections/input/Input.vue';
 import LvButton from '@/collections/buttons/Button.vue';
+import VueGtag from 'vue-gtag';
+
+Vue.use(VueGtag, {
+    config: { id: 'G-NSCV9YG71N' },
+    appName: 'lightvue-nuxt',
+});
 
 Vue.component('LvInput', LvInput);
 Vue.component('LvButton', LvButton);
