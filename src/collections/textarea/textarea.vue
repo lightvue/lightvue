9c568f7f4c2e408c2ce94aad765d78504ee3903a
Pulkit <pulkit.cloud@gmail.com>
2021-02-15 17:47:49 +0530

    FIXED: Text-area char limits.

--- src/collections/textarea/textarea.vue
+++ src/collections/textarea/textarea.vue
@@ -1,11 +1,10 @@
 <template>
   <div>
     <lv-input type="text" :editable="false" ref="mainInput" v-bind="$attrs">
-            <textarea :class="`p-inputtextarea-resizable : ${autoResize}`" :maxlength="maxlength" class="lv-textarea" @keyup='remaincharCount()' v-bind="$attrs"  @input="this.updateValue" :value="modelValue" ref="textarea"/>
+      <textarea :class="`p-inputtextarea-resizable : ${autoResize}`" :maxlength="maxlength" class="lv-textarea" @keyup="remaincharCount()" v-bind="$attrs" @input="this.updateValue" :value="modelValue" ref="textarea" />
       <!-- <textarea v-bind="$attrs" class="lv-textarea" :class=" {'p-inputtextarea-resizable ': autoResize}" @input="this.updateValue" ref="textarea"></textarea> -->
     </lv-input>
-    <!-- <span class="textarea__characters">{{ remaincharactersText }}</span> -->
-    <div class="textarea__characters"> {{modalValue}} /  {{maxcharacter}}</div>
+    <div class="lv-textarea__limits" v-if="showLimit && maxLength">{{ modelValue ? modelValue.length : 0 }} / {{ maxLength }}</div>
   </div>
 </template>
 
@@ -13,109 +12,117 @@
 import LvInput from '@/collections/input/Input.vue';
 
 export default {
-    name: 'LvTextarea',
-    components: {
-        LvInput,
+  name: 'LvTextarea',
+  components: {
+    LvInput,
+  },
+  data() {
+    return {
+      localValue: '',
+    };
+  },
+  props: {
+    value: {
+      type: String,
+      default: '',
     },
-    props: {
-          value: {
-            type: String,
-            default: ""
-        },
-        remaincharactersText: {
-            type: String,
-            default: "0/20"
-        },
-        maxlength: {
-            type: Number,
-            default: "20",
-        }
+    remaincharactersText: {
+      type: String,
+      default: '0/20',
     },
-    cachedScrollHeight: null,
-    mounted() {
-        if (this.$refs.textarea.offsetParent && this.autoResize) {
-            this.resize();
-        }
+    maxLength: {
+      type: Number,
     },
-    updated() {
-        if (this.$refs.textarea.offsetParent && this.autoResize) {
-            this.resize();
-        }
+    showLimit: {
+      type: Boolean,
+      default: false,
     },
-    methods: {
-        resize() {
-            if (!this.cachedScrollHeight) {
-                this.cachedScrollHeight = this.$refs.textarea.scrollHeight;
-                this.$refs.textarea.style.overflow = "hidden";
-            }
+  },
+  cachedScrollHeight: null,
+  mounted() {
+    if (this.$refs.textarea.offsetParent && this.autoResize) {
+      this.resize();
+    }
+  },
+  updated() {
+    if (this.$refs.textarea.offsetParent && this.autoResize) {
+      this.resize();
+    }
+  },
+  methods: {
+    resize() {
+      if (!this.cachedScrollHeight) {
+        this.cachedScrollHeight = this.$refs.textarea.scrollHeight;
+        this.$refs.textarea.style.overflow = 'hidden';
+      }
 
-            if (this.cachedScrollHeight !== this.$refs.textarea.scrollHeight) {
-                this.$refs.textarea.style.height = ''
-                this.$refs.textarea.style.height = this.$refs.textarea.scrollHeight + 'px';
+      if (this.cachedScrollHeight !== this.$refs.textarea.scrollHeight) {
+        this.$refs.textarea.style.height = '';
+        this.$refs.textarea.style.height = this.$refs.textarea.scrollHeight + 'px';
 
-                if (parseFloat(this.$refs.textarea.style.height) >= parseFloat(this.$refs.textarea.style.maxHeight)) {
-                    this.$refs.textarea.style.overflowY = "scroll";
-                    this.$refs.textarea.style.height = this.$refs.textarea.style.maxHeight;
-                }
-                else {
-                    this.$refs.textarea.style.overflow = "hidden";
-                }
+        if (parseFloat(this.$refs.textarea.style.height) >= parseFloat(this.$refs.textarea.style.maxHeight)) {
+          this.$refs.textarea.style.overflowY = 'scroll';
+          this.$refs.textarea.style.height = this.$refs.textarea.style.maxHeight;
+        } else {
+          this.$refs.textarea.style.overflow = 'hidden';
+        }
 
-                this.cachedScrollHeight = this.$refs.textarea.scrollHeight;
-            }
-        },
-          updateValue(event) {
+        this.cachedScrollHeight = this.$refs.textarea.scrollHeight;
+      }
+    },
+    updateValue(event) {
+      this.localValue = event.target.value;
       this.$emit('input-native', event);
       this.$emit('input', event.target.value); // Only for Vue 2
       this.$emit('update:modelValue', event.target.value); // Only for Vue 3
     },
-        //  remaincharCount: function(){
+    //  remaincharCount: function(){
 
-        //     if(this.message.length > this.maxcharacter){
-        //         this.remaincharactersText = "Exceeded "+this.maxcharacter+" characters limit.";
-        //     }
-        //     else{
-        //     //    var remainCharacters = this.maxcharacter - this.message.length;
-        //        var remainCharacters = this.message.length;
-        //        this.remaincharactersText = "" + remainCharacters + "/" + this.maxcharacter;
-        //     }
-        // }
+    //     if(this.message.length > this.maxcharacter){
+    //         this.remaincharactersText = "Exceeded "+this.maxcharacter+" characters limit.";
+    //     }
+    //     else{
+    //     //    var remainCharacters = this.maxcharacter - this.message.length;
+    //        var remainCharacters = this.message.length;
+    //        this.remaincharactersText = "" + remainCharacters + "/" + this.maxcharacter;
+    //     }
+    // }
+  },
+  computed: {
+    listeners() {
+      return {
+        ...this.$listeners,
+        input: event => {
+          if (this.autoResize) {
+            this.resize();
+          }
+
+          this.$emit('input', event.target.value);
+        },
+      };
     },
-    computed: {
-        listeners() {
-            return {
-                ...this.$listeners,
-                input: event => {
-                    if (this.autoResize) {
-                        this.resize();
-                    }
-
-                    this.$emit('input', event.target.value);
-                }
-            };
-        },
-        filled() {
-            return (this.value != null && this.value.toString().length > 0)
-        },
-        modelValue() {
-            return this.$attrs.modelValue ? this.$attrs.modelValue : this.value;
-        },
-    }
+    filled() {
+      return this.value != null && this.value.toString().length > 0;
+    },
+    modelValue() {
+      return this.$attrs.modelValue ? this.$attrs.modelValue : this.value ? this.value : this.localValue;
+    },
+  },
 };
 </script>
 
 <style lang="scss">
-textarea.lv-textarea{
-    border: none;
-    width: 100%;
-    background: transparent;
-    outline: none;
-    resize: vertical;
+.lv-textarea {
+  border: none;
+  width: 100%;
+  background: transparent;
+  outline: none;
+  resize: vertical;
 }
-span.textarea__characters{
-    display: flex;
-    justify-content: flex-end;
-    margin-top: 5px;
+.lv-textarea__limits {
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 5px;
 }
-</style>>
-
+</style>
+>
