e9e5c1fe5c790a67e6f1709134131eb3bd2bb670
aankushh <ankushkhandelwal67@gmail.com>
2021-02-16 17:28:27 +0530

    Chgeckboxgroup

---
+++ src/collections/checkbox-group/ChechboxGroup.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <lv-check-box v-for="option in options" :key="option">{{ option }}</lv-check-box>
+  </div>
+</template>
+<script>
+import LvCheckBox from '@/collections/checkbox/Checkbox';
+export default {
+  components: {
+    LvCheckBox,
+  },
+  props: ['options'],
+};
+</script>
