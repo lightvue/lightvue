a710e4f0ba7c80477bf9998a1761a5d059de7eb0
Ishaan Mehta <ishaanmehta4@gmail.com>
2021-02-17 15:56:56 +0530

    Modified tooltip demo

--- src/examples/tooltip/TooltipFocusDemo.vue
+++ src/examples/tooltip/TooltipFocusDemo.vue
@@ -1,10 +1,13 @@
 <template>
         <div class="tooltip-example-wrapper">
-            <div>
+            <!-- <div>
                 <LvInput type="text" placeholder="Click to focus" v-tooltip.bottom.focus="'Tooltip message'" />
+            </div> -->
+            <div>
+                <input type="text" placeholder="Click to focus" v-tooltip.bottom.focus="'Tooltip message'" />
             </div>
             <div>
-                <LvInput type="text" placeholder="Click to focus" v-tooltip.top.focus="'Tooltip message'" />
+                <input type="text" placeholder="Click to focus" v-tooltip.top.focus="'Tooltip message'" />
             </div>
         </div>
 </template>
@@ -27,4 +30,24 @@ export default {
     margin-left: 100px;
     margin-bottom: 60px;
 }
+
+.tooltip-example-wrapper > div input {
+    padding: 8px;
+    background-color: #edf2f7;
+    border: none;
+    width: 100%;
+    border-radius: 4px;
+    border: solid 3px rgba(0,0,0,0);
+    transition: all 0.2s;
+}
+
+.tooltip-example-wrapper > div input::placeholder {
+    opacity: 0.7;
+}
+
+.tooltip-example-wrapper > div input:focus {
+    outline: none;
+    border: solid 3px rgba(0,0,0,0.1);
+}
+
 </style>
