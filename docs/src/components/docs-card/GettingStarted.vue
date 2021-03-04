d6b650bbc256ab9d3442d7beadd48d1c997dd644
Pulkit <pulkit.cloud@gmail.com>
2021-03-05 01:41:14 +0530

    FIXED: Internal getting started

--- docs/src/components/docs-card/GettingStarted.vue
+++ docs/src/components/docs-card/GettingStarted.vue
@@ -1,119 +1,73 @@
 <template>
-  <div class="install__wrapper">
-    <div class="install__container">
-      <h1>Getting Started</h1>
-      <p>In order to get started with light vue, you need to install LightVue and its dependency LightIcon (Only required if the icons are getting used).</p>
-      <h2>1. Installing LightVue</h2>
-      <div class="install__process">
-        <CodeHighlight lang="javascript">
-          <!-- <pre> -->
-          npm install lightvue
-          <!-- </pre> -->
-        </CodeHighlight>
+  <docs-card title="Getting Started" :responsive="false">
+    <div class="getting-started">
+      <div class="docs-step">
+        <code-highlight lang="javascript">
+          <pre>
+// import the component
+import {{ componentName }} from 'lightvue/{{ packageName }}';
+</pre
+          >
+        </code-highlight>
       </div>
-      <p>OR</p>
-      <div class="install__process">
-        <CodeHighlight lang="javascript">
-          <!-- <pre> -->
-          yarn add lightvue
-          <!-- </pre> -->
-        </CodeHighlight>
+      <div class="docs-step">
+        <code-highlight lang="javascript">
+          <pre>
+// Register the component locally
+export default {
+    components: {
+        {{ componentName }}
+    }
+}</pre
+          >
+        </code-highlight>
       </div>
-      <h3>With Vue CLI Plugin</h3>
-      <p>While using Vue CLI, LighgVue can be installed using the LightVue plugin</p>
-      <div class="install__process">
-        <CodeHighlight lang="javascript">
-          <!-- <pre> -->
-          vue add light-vue
-          <!-- </pre> -->
-        </CodeHighlight>
-      </div>
-      <h2>2. Configuring your Vue Project</h2>
-      <div class="install__process">
-        <CodeHighlight lang="javascript">
-          <!-- <pre> -->
-          import Vue from 'vue' <br />
-          import LightVue from 'light-vue' <br />
-          <br />
-          const components = { <br />
-          //... <br />
-          } <br />
-          Vue.use(LightVue, components)
-          <!-- </pre> -->
-        </CodeHighlight>
-      </div>
-      <h3>Injecting Global Styles</h3>
-      <div class="install__process">
-        <CodeHighlight lang="javascript">
-          <!-- <pre> -->
-          import Vue from 'vue' <br />
-          import LightVue, { LvThemeProvider, LvReset } from '@lightvue/vue' <br />
-          import App from './App.vue' <br />
-          Vue.use(LightVue) <br />
-          new Vue({ <br />
-          el: '#app', <br />
-          render: (h) => h(LvThemeProvider, [h(LvReset), h(App)]) <br />
-          }).$mount()
-          <!-- </pre> -->
-        </CodeHighlight>
-      </div>
-      <h3>Injecting the required components</h3>
-      <div class="install__process">
-        <CodeHighlight lang="javascript">
-          <!-- <pre> -->
-          import Vue from 'vue' <br />
-          import LightVue from 'light-vue' <br />
+      <div class="docs-step">
+        <code-highlight lang="javascript">
+          <pre>
+// Register the component globally (Vue 2.x)
+Vue.component('{{ componentName }}', {{ componentName }});
 
-          import LvInput from 'lightvue/vue-components/lv-input' <br />
-          import lvButton from 'lightvue/vue-components/lv-button' <br />
-
-          const settings = { <br />
-          'lv-input': {<br />
-          component: LvInput, <br />
-          props: { <br />
-
-          } <br />
-          }, <br />
-          'lv-button': { <br />
-          component: LvButton, <br />
-          props: { <br />
-
-          } <br />
-          }, <br />
-          } <br />
-
-          Vue.use(LightVue, settings)
-          <!-- </pre> -->
-        </CodeHighlight>
+// Register the component globally (Vue 3.x)
+const app = Vue.createApp({
+  // ...
+});
+app.component('{{ componentName }}', {{ componentName }});
+app.mount('#app')
+               </pre
+          >
+        </code-highlight>
       </div>
+
+      <slot></slot>
     </div>
-  </div>
+  </docs-card>
 </template>
 
 <script>
-import CodeHighlight from '@/components/docs-card/CodeHighlight.vue';
+import DocsCard from './DocsCard.vue';
+import CodeHighlight from './CodeHighlight.vue';
+
 export default {
-  components: { CodeHighlight },
+  components: {
+    DocsCard,
+    CodeHighlight,
+  },
+  props: {
+    packageName: {
+      type: String,
+      required: true,
+    },
+    componentName: {
+      type: String,
+      required: true,
+    },
+  },
 };
 </script>
 
-<style lang="scss" scoped>
-// .install__wrapper {
-//   padding: 2rem 8rem;
-// }
-.install__container {
-  h1 {
-    text-align: center;
-    font-size: 3.5rem;
-    font-weight: 700;
-    line-height: 1.5;
-    margin: 50px 0;
-    color: #585858;
-  }
-  p {
-    margin-top: 1rem;
-    line-height: 1.625;
-    font-size: 16px;
-  }
+<style scoped>
+.docs-step {
+  border-top: 2px dashed #dee7ea;
 }
 </style>
