7e68b5fb027f6e0336231683607f6eaf84cd3483
Prakhar Jaiman <jaimanprakhar12345@gmail.com>
2021-03-06 22:04:37 +0530

    .

--- core/components/notification/Notification.d.ts
+++ core/components/notification/Notification.d.ts
@@ -1,6 +1,10 @@
 import Vue from 'vue';
-
+import { createApp } from 'vue';
 export declare class Toast extends Vue {
-    group?: string;
-    position?: string;
+  group?: string;
+  position?: string;
+}
+export declare class Toast extends createApp {
+  group?: string;
+  position?: string;
 }
