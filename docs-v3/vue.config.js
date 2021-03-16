b68c851a43ebe36ca650955000c5bf4cfced1fa8
Prakhar Jaiman <jaimanprakhar12345@gmail.com>
2021-03-16 15:39:10 +0530

    .

--- docs-v3/vue.config.js
+++ docs-v3/vue.config.js
@@ -4,17 +4,17 @@ const pkg = require('../package.json');
 module.exports = {
   configureWebpack: {
     resolve: {
-      alias: {
-        '@': path.join(__dirname, '/../docs/src'),
-        'lightvue/tooltip': path.join(__dirname, '/../core/directives/tooltip'),
-        'lightvue/ripple': path.join(__dirname, '/../core/directives/ripple'),
-        'lightvue/utils': path.join(__dirname, '/../core/utils'),
-        'lightvue/example': path.join(__dirname, '/../examples'),
-        'lightvue/mixin': path.join(__dirname, '/../core/mixins'),
-        lightvue: path.join(__dirname, '/../core/components'),
-        splitpanes: path.join(__dirname, '/src/components/splitpanes'),
-        // '@@@': path.join(__dirname, '/..'), // this must never be used, resulting infinite loop.
-      },
+      // alias: {
+      //   '@': path.join(__dirname, '/../docs/src'),
+      //   'lightvue/tooltip': path.join(__dirname, '/../core/directives/tooltip'),
+      //   'lightvue/ripple': path.join(__dirname, '/../core/directives/ripple'),
+      //   'lightvue/utils': path.join(__dirname, '/../core/utils'),
+      //   'lightvue/example': path.join(__dirname, '/../examples'),
+      //   'lightvue/mixin': path.join(__dirname, '/../core/mixins'),
+      //   lightvue: path.join(__dirname, '/../core/components'),
+      //   splitpanes: path.join(__dirname, '/src/components/splitpanes'),
+      //   '@@@': path.join(__dirname, '/..'), // this must never be used, resulting infinite loop.
+      // },
     },
   },
   chainWebpack: config => {
