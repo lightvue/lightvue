f5c5037329e03e97e2cdf1cf5f92716c9f67afdb
Pulkit <pulkit.cloud@gmail.com>
2021-03-16 10:19:08 +0530

    FIXED: path for lightvue/ripple/rippleAPI

--- docs/src/pages/vue-components/ripple.vue
+++ docs/src/pages/vue-components/ripple.vue
@@ -15,7 +15,7 @@ import DocsCardVue from '@/components/docs-card/DocsCardVue.vue';
 import DocsCard from '@/components/docs-card/DocsCard.vue';
 import GettingStarted from '@/components/docs-card/GettingStarted.vue';
 import DocsAllApi from '@/components/docs-card/DocsAllApi.vue';
-import RippleAPI from 'lightvue/rippleAPI';
+import RippleAPI from 'lightvue/ripple/rippleAPI';
 
 export default {
   components: {
