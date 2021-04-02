4e149bc5db9a295968495fe4aa4d1fc0adca9463
aankushh <ankushkhandelwal67@gmail.com>
2021-04-03 00:32:07 +0530

    resolved skeleton api

--- examples/shimmers/ShimmersAPI.js
+++ examples/shimmers/ShimmersAPI.js
@@ -1,29 +1,77 @@
-const ProgressLoadersAPI = {
-    props:[
-        {
-            name: 'value',
-            type: 'Number',
-            default: '0',
-            description: 'Current progress in %, applicable only in determinate mode.',
-        },
-        {
-            name: 'mode',
-            type: 'String',
-            default: 'determinate',
-            description: 'Type of progress bar required, "determinate" or "indeterminate".',
-        },
-        {
-            name: 'color',
-            type: 'String',
-            default: '#607c8a',
-            description: 'Color of the progress bar.',
-        },
-        {
-            name: 'showValue',
-            type: 'Boolean',
-            default: 'true',
-            description: 'Choose if you want to show or hide the current % of progress',
-        },
-    ],
-}
-export default ProgressLoadersAPI;
+const ShimmersAPI = {
+  props: [
+    {
+      name: 'width',
+      type: 'Number',
+      default: '',
+      description: 'Defines width of the Skeleton.',
+    },
+    {
+      name: 'height',
+      type: 'Number',
+      default: '',
+      description: 'Defines height of the Skeleton.',
+    },
+    {
+      name: 'viewBox',
+      type: 'String',
+      default: '',
+      description: 'Defines the position and dimension, in user space, of an SVG viewport. ',
+    },
+    {
+      name: 'preserveAspectRatio',
+      type: 'String',
+      default: '',
+      description: 'maintains the aspect ratio.',
+    },
+    {
+      name: 'speed',
+      type: 'Number',
+      default: '2',
+      description: 'Defines speed of the animation.',
+    },
+    {
+      name: 'baseUrl',
+      type: 'String',
+      default: '',
+      description: '',
+    },
+    {
+      name: 'primaryColor',
+      type: 'String',
+      default: '#f9f9f9',
+      description: 'Defines the primary Color of the Skeleton.',
+    },
+    {
+      name: 'secondaryColor',
+      type: 'String',
+      default: '#ecebeb',
+      description: 'Defines the secondary Color of the Skeleton.',
+    },
+    {
+      name: 'primaryOpacity',
+      type: 'Number',
+      default: '1',
+      description: 'Defines opacity of the primary color',
+    },
+    {
+      name: 'secondaryOpacity',
+      type: 'Number',
+      default: '1',
+      description: 'Defines opacity of the secondary color',
+    },
+    {
+      name: 'uniqueKey',
+      type: 'String',
+      default: '',
+      description: 'Defines unique key of the skeleton',
+    },
+    {
+      name: 'animate',
+      type: 'Boolean',
+      default: '',
+      description: 'Allows to animate the Skeleton',
+    },
+  ],
+};
+export default ShimmersAPI;
