fe98bb58d847543f78e93d975aff3744664d8f17
Pulkit <pulkit.cloud@gmail.com>
2021-04-03 02:17:00 +0530

    Revert: NPM Testing paths.

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -112,13 +112,13 @@ export default {
     ALGOLIA_PROJECT_ID: process.env.ALGOLIA_PROJECT_ID,
   },
   alias: {
-    // 'lightvue/tooltip': path.join(__dirname, '/../core/directives/tooltip'),
-    // 'lightvue/ripple': path.join(__dirname, '/../core/directives/ripple'),
-    // 'lightvue/utils': path.join(__dirname, '/../core/utils'),
-    // 'lightvue/mixins': path.join(__dirname, '/../core/mixins'),
+    'lightvue/tooltip': path.join(__dirname, '/../core/directives/tooltip'),
+    'lightvue/ripple': path.join(__dirname, '/../core/directives/ripple'),
+    'lightvue/utils': path.join(__dirname, '/../core/utils'),
+    'lightvue/mixins': path.join(__dirname, '/../core/mixins'),
     'lightvueDocs/example': path.join(__dirname, '/../examples'),
-    // 'lightvueDocs/example': path.join(__dirname, '/../examples'),
-    //   lightvue: path.join(__dirname, '/../core/components'),
+    'lightvueDocs/example': path.join(__dirname, '/../examples'),
+    lightvue: path.join(__dirname, '/../core/components'),
     //   // '@@@': path.join(__dirname, '/..'), // this must never be used, resulting infinite loop.
   },
 };
