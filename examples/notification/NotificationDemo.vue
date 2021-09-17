2c32334138fd80fa581193fd6a041842d095c240
Pulkit <pulkit.cloud@gmail.com>
2021-09-18 04:28:50 +0530

    ADDED: Scoped css in toast demo.

--- examples/notification/NotificationDemo.vue
+++ examples/notification/NotificationDemo.vue
@@ -58,7 +58,7 @@ export default {
   },
 };
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
 .--mr-4 {
   margin-right: 8px;
   margin-top: 3px;
--- examples/toast/ToastDemo.vue
+++ examples/toast/ToastDemo.vue
@@ -62,7 +62,7 @@ export default {
   },
 };
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
 .--mr-4 {
   margin-right: 8px;
 }
