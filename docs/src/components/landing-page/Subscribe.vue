7b38ae3014ec1370cb67720d09bbfc108a9ef25d
Dhairya <Dhairyagupta0001@gmail.com>
2021-10-02 18:47:44 +0530

    WIP: Subscribe Card

--- docs/src/components/landing-page/Subscribe.vue
+++ docs/src/components/landing-page/Subscribe.vue
@@ -4,9 +4,15 @@
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" style="margin-bottom: -8px"><path fill="#143a56" fill-opacity="1" d="M0,128L120,117.3C240,107,480,85,720,85.3C960,85,1200,107,1320,117.3L1440,128L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"></path></svg>
       <div class="subscribe-cover__bg-blank"></div>
     </div>
-    <div class="subscribe-cover__illustration">
-      <img src="/hero-bg.svg" alt="LightVue Illustration" />
+    <div class="container">
+      <h1 class="first"><strong>Ready To Get Started ?</strong></h1>
+      <p id="first">Talk to us today!</p>
+      <div class="btn">Subscribe</div>
     </div>
+    <!-- <div class="subscribe-cover__illustration">
+      <img src="/hero-bg.svg" alt="LightVue Illustration" />
+    </div> -->
+    <div class="subscribe-card">Subscribe</div>
   </div>
 </template>
 
@@ -31,14 +37,45 @@ export default {};
     width: 100%;
     background-color: #143a56;
   }
-  .subscribe-cover__illustration {
+  .subscribe-card {
     display: flex;
     justify-content: center;
-    img {
-      max-height: 320px;
-      max-width: 100vw;
-      object-fit: contain;
-      z-index: 2;
+    height: 200px;
+    width: 100px;
+    border-radius: 50%;
+  }
+  .container {
+    .first {
+      justify-content: space-between;
+      background-color: rgb(255, 255, 255);
+      border: none;
+      padding: 30px;
+      margin: 40px;
+      border-radius: 40px;
+      width: 800px;
+      height: 200px;
+      font-size: 27px;
+      margin: auto;
+      color: #008080;
+      box-shadow: 0 0 10px 5px;
+      text-align: center;
+    }
+
+    .btn {
+      background-color: #008080;
+      color: white;
+      width: 130px;
+      border-radius: 50%;
+      font-size: 20px;
+      margin: auto;
+      text-align: center;
+    }
+
+    #second {
+      margin: auto;
+      font-size: 30px;
+      color: black;
+      text-align: right;
     }
   }
 }
