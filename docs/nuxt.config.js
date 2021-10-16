c65e1d249aa4aa9ae0143c6ebdee283e23ba3be7
Pulkit <pulkit.cloud@gmail.com>
2021-10-17 00:27:32 +0530

    uncommented: directives.

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -47,7 +47,7 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: ['@/assets/styles/lightvue.scss'],
+  css: ['@/assets/styles/lightvue.scss', 'lightvue/lightvue.css'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: ['plugins/global.js'],
