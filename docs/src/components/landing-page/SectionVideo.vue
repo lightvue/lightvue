74e0719746cf4db1d4893c4b9afcd8bb08cf3301
hrushi-thorat <hrushikeshst54@gmail.com>
2021-10-26 13:27:27 +0530

    Improve:videographer Svg

--- docs/src/components/landing-page/SectionVideo.vue
+++ docs/src/components/landing-page/SectionVideo.vue
@@ -2,7 +2,7 @@
   <section class="video-section">
     <div class="light--wrapper">
       <div class="light-image--wrapper">
-        <img src="../../static/elements/undraw_videographer_nnc7.svg" alt="camera man svg" class="vue-video__image" />
+        <img src="../../static/elements/videographer.svg" alt="camera man svg" class="vue-video__image" />
       </div>
       <div class="light-video">
         <iframe width="600" height="300" src="https://www.youtube.com/embed/fjYPDdf4rGA?rel=0" title="LightVue introduction Video" frameborder="0"></iframe>
