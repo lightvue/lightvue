ccdf2db519eb15cc08ba1cfc52262fddcd15ef61
dhairya-0001 <91007588+dhairya-0001@users.noreply.github.com>
2021-10-31 17:42:05 +0530

    Improved: Description in features section oh Landing Page

--- docs/src/components/landing-page/SectionFeatures.vue
+++ docs/src/components/landing-page/SectionFeatures.vue
@@ -12,7 +12,7 @@
         <div class="section-features__content">
           <div class="section-features__content-heading">The LightVue</div>
           <br />
-          <div class="section-features__content-description">UI Library hands out everything you need to create contemporary, engaging, and responsive web applications. A platform set to offer unlimited light customizable UI variants and an implementation playground to define how your components would look to and fro.</div>
+          <div class="section-features__content-description">LightVue is the most emerging UI component Library that hands out everything you need to create contemporary, engaging, and responsive web applications. A collection of well crafted components to offer unlimited light customizable UI variants and an implementation playground to define how your components would look to and fro.</div>
           <br /><br />
           <nuxt-link :to="{ hash: 'vue-migrate' }" class="page-button --dark">Know More</nuxt-link>
           <!-- <lv-button :push="true" :deep-shadow="true" label="Know More" type="button" size="xl" class="section-button__customise" style="--theme-color: #143a56; --theme-color-dark: #194a70" /> -->
