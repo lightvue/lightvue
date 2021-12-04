92271ae0a01e17b2948340c2af1515e082fca324
AnupamKris <anupamkris13262@gmail.com>
2021-12-04 20:12:59 +0530

    Masked Input init

---
+++ core/components/masked-input/MaskedInput.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="lv-masked-input__group">
+    <input class="lv-masked-input__element" type="text" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+@import './MaskedInput.scss';
+</style>
