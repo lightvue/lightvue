bfe79a09ada66cf6f37960597ddeadf380b0b6ef
hrushi-thorat <hrushikeshst54@gmail.com>
2021-12-11 13:56:00 +0530

    Update:Drag Demo

--- examples/upload/UploadDragDemo.vue
+++ examples/upload/UploadDragDemo.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
-    <LvUpload accept-extensions=".jpg,.svg" :multiple="true" :max-file-size="5 * 1024 * 1024" @validated="handleFilesValidated" @changed="handleFilesChanged" drop height="100%" width="100%">
-      <div class="upload">
+    <LvUpload :max-file-size="5000" extensions=".png" @submit-image="handleFilesChanged($event)" @file-size-error="fileSizeError" @extension-error="showError" drop height="100%" width="100%">
+      <div class="upload" v-if="!filePreviewImage.length > 0">
         <i class="light-icon-cloud-upload"></i>
+      </div>
+      <div class="page__demo-preview" v-else>
+        <img alt="filePreviewName" class="page__demo-preview-image" :src="filePreviewImage" />
       </div>
     </LvUpload>
     <!-- <LvUpload accept-extensions=".jpg,.svg" :multiple="true" :max-file-size="5 * 1024 * 1024" @validated="handleFilesValidated" @changed="handleFilesChanged" width="100%">
@@ -18,14 +21,29 @@ export default {
   components: {
     LvUpload,
   },
+  data() {
+    return {
+      filePreviewImage: '',
+      fileName: '',
+    };
+  },
   methods: {
-    handleFilesValidated(result, files) {
-      console.log('Validation result: ', result);
+    showError() {
+      console.log('hii');
+      this.$notification.add({ type: 'error', title: 'File Size Error', content: 'Please select Valid File Type' });
+    },
+    fileSizeError() {
+      window.alert('file to too big');
+    },
+    handleFilesChanged(files) {
+      this.fileName = files[0].name;
+      this.$notification.add({ type: 'success', title: 'File Uploaded', content: 'File Upload success', position: 'bottom-right', duration: 3000 });
+      this.handleFilesChangedPreview(files);
     },
 
-    handleFilesChanged(files) {
+    handleFilesChangedPreview(files) {
       console.log('Selected files: ');
-      console.table(files);
+      this.filePreviewImage = URL.createObjectURL(files[0]);
     },
   },
 };
