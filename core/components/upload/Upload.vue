c4ac765e86321426e81e057503f7f2dda2e17d34
hrushi-thorat <hrushikeshst54@gmail.com>
2021-12-11 13:11:14 +0530

    Improve:Upload validation

--- core/components/upload/Upload.vue
+++ core/components/upload/Upload.vue
@@ -4,28 +4,16 @@
       <LvButton :icon="icon" :class="ButtonClass" @click="openPicker" :rounded="rounded" :label="placeholder" />
     </template>
     <template v-else-if="!drop && withButtonInput">
-      <!-- <div class="fileupload__content">
-        <div class="fileupload__icon">
-          <slot name="icon">
-            <i class="light-icon-cloud-upload"></i>
-          </slot>
-        </div>
-
-        <span v-if="hasSelection" class="fileupload__display-text">{{ displayText }}</span>
-        <slot v-else>{{ placeholder }}</slot>
-      </div> -->
-      <!-- <input class="md-input" readonly v-bind="{ placeholder }" @click="openPicker" /> -->
       <lv-input type="text" v-model="value1" :placeholder="placeholder" clearable @click="openPicker" :rounded="rounded">
         <template #prepend>
           <LvButton :icon="icon" :class="ButtonClass" @click="openPicker" />
         </template>
       </lv-input>
     </template>
-
     <div v-else class="file--upload__draggable" ref="LvDroppable" @dragenter.prevent="isDragEnter = true" @dragover.prevent="() => {}" @dragleave.prevent="isDragEnter = false" @drop.prevent="handleDrop" :style="computedStyle">
       <slot></slot>
     </div>
-    <input ref="LvFileInput" type="file" :tabindex="tabindex" :multiple="multiple" :accept="acceptExtensions" @change="handleChange" />
+    <input ref="LvFileInput" type="file" :accept="extensions" :tabindex="tabindex" :multiple="multiple" @change="handleFileChange($event)" />
   </label>
 </template>
 <script>
@@ -66,16 +54,19 @@ export default {
       type: Boolean,
       default: false,
     },
-
-    acceptExtensions: {
+    // Same as native accept attribute
+    extensions: {
       type: String,
-      default: '',
+      default: '.jpg,.svg',
     },
 
+    // This value should be a positive number
     maxFileSize: {
-      // in bytes
       type: Number,
-      default: NaN,
+      default: 1024,
+      validator: value => {
+        return value > 0;
+      },
     },
 
     height: {
@@ -123,53 +114,40 @@ export default {
     openPicker() {
       this.$refs.LvFileInput.click();
     },
-    handleChange($event) {
-      this.onFileSelected($event);
-      this.preprocessFiles($event.target.files);
-    },
-
-    onFileSelected({ target, dataTransfer }) {
-      const files = target.files || dataTransfer.files;
-
-      this.value1 = this.getFileName(files, target);
-    },
-    getFileName(files, target) {
-      if (!files || files.length === 0) {
-        return target.value.split('\\').pop();
+    handleFileChange(e) {
+      // This method will be called when the file gets uploaded
+      const files = e.target.files || e.dataTransfer.files;
+      // We are checking the size of image or if not valid then calling sizeErrorCallback
+      const imageSize = files[0].size / 1024; // size in KB
+      console.log(imageSize);
+      console.log(imageSize > this.maxFileSize);
+      if (imageSize > this.maxFileSize) {
+        this.$emit('file-size-error', imageSize);
+        this.$refs.LvFileInput.value = '';
+        return;
       }
-
-      if (files.length > 1) {
-        return this.getMultipleName(files);
+      // check File extension for normal upload its not necessary but for drop we have to check the extension
+      if (!this.checkFileExtensions(files)) {
+        this.$emit('extension-error', files);
+        this.$refs.LvFileInput.value = '';
+        return;
       }
-
-      if (files.length === 1) {
-        return files[0].name;
+      if (files && files[0]) {
+        this.$emit('submit-image', files);
       }
-
-      return null;
     },
-    getMultipleName(files) {
-      let names = [];
-
-      [...files].forEach(({ name }) => names.push(name));
-
-      return names.join(', ');
-    },
-
     handleDrop($event) {
       this.isDragEnter = false;
-      this.preprocessFiles($event.dataTransfer.files);
+      this.handleFileChange($event);
     },
 
     checkFileExtensions(files) {
       // get non-empty, unique extension items
-      const extList = [...new Set(this.acceptExtensions.toLowerCase().split(',').filter(Boolean))];
+      const extList = [...new Set(this.extensions.toLowerCase().split(',').filter(Boolean))];
       const list = Array.from(files);
-
       // check if the selected files are in supported extensions
       const invalidFileIndex = list.findIndex(file => {
         const ext = `.${file.name.toLowerCase().split('.').pop()}`;
-
         return !extList.includes(ext);
       });
 
@@ -177,51 +155,18 @@ export default {
       return invalidFileIndex === -1;
     },
 
-    checkFileSize(files) {
-      if (Number.isNaN(this.maxFileSize)) {
-        return true;
-      }
-
-      const list = Array.from(files);
-
-      // find invalid file size
-      const invalidFileIndex = list.findIndex(file => file.size > this.maxFileSize);
-
-      // all file size are valid
-      return invalidFileIndex === -1;
-    },
-
     validate(files) {
       // file selection
       if (!this.multiple && files.length > 1) {
-        return 'MULTIFILES_ERROR';
+        return this.$emit('multiple-file-error');
       }
-
-      // extension
-      if (!this.checkFileExtensions(files)) {
-        return 'EXTENSION_ERROR';
-      }
-
-      // file size
-      if (!this.checkFileSize(files)) {
-        return 'FILE_SIZE_ERROR';
-      }
-
-      // custom validation
-      return this.validateFn(files);
     },
-
-    preprocessFiles(files) {
-      const result = this.validate(files);
-      this.$emit('validated', result, files);
-
-      // validation
-      if (result === true) {
-        this.$emit('changed', files);
-      }
-
-      // clear selected files
-      this.$refs.LvFileInput.value = '';
+    getImageURL(image) {
+      // here we are dealing with variuos cases
+      // 1: if image is not provided use default image from assets
+      // 2: if image is string use it directly
+      // 3: if image is dataURL object, create image url using data, encoding and data format
+      return image ? (typeof image === 'object' ? `data:image/${image.dataFormat};${image.encoding},${image.data}` : image) : this.defaultImage;
     },
   },
 };
