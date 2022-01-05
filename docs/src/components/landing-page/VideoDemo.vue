f67706ea7ea1d4e5b78f48e8171ef67b968cb5d4
Pragati jain <jpragati05@gmail.com>
2022-01-05 10:40:18 +0530

    Video_demo

---
+++ docs/src/components/landing-page/VideoDemo.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="video-section">
+    <div class="light--wrapper">
+      <div class="light-image--wrapper">
+        <!-- <img src="../../static/elements/videographer.svg" alt="camera man svg" class="vue-video__image" /> -->
+      </div>
+      <div class="light-video">
+        <iframe width="600" height="300" src="https://www.youtube.com/embed/fjYPDdf4rGA?rel=0" title="LightVue introduction Video" frameborder="0"></iframe>
+      </div>
+    </div>
+  </section>
+</template>
