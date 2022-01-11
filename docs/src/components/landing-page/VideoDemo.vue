3be72e168009469462cfe39ba9f341cd846a0d94
Pragati jain <jpragati05@gmail.com>
2022-01-11 20:03:36 +0530

    Modal for video

--- docs/src/components/landing-page/VideoDemo.vue
+++ docs/src/components/landing-page/VideoDemo.vue
@@ -1,109 +1,108 @@
-<script>
-  export default {
-    name: 'modal',
-    methods: {
-      close() {
-        this.$emit('close');
-      },
-    },
-  };
-</script>
 <template>
-  <transition name="modal-fade">
-    <div class="modal-backdrop">
-      <div class="modal"
-        role="dialog"
-        aria-labelledby="modalTitle"
-        aria-describedby="modalDescription"
-      >
-     
-        <header
-          class="modal-header"
-          id="modalTitle"
-        >
-          <slot name="header">
+<div id="app">
+    <button id="btn1" class="button" @click="showmodal = true"></button>
+    <transition name="fade" appear>
+        <div class="modal-overlay" v-if="showmodal" @click="showmodal = false">
+        </div>
+    </transition>
+    <transition name="slide" appear>
+        <div class="modal" v-if="showmodal" >
             <iframe width="600" height="300" src="https://www.youtube.com/embed/fjYPDdf4rGA?rel=0" title="LightVue introduction Video" frameborder="0"></iframe>
-          </slot>
-
-          <button
-            type="button"
-            class="btn-close"
-            @click="$emit('close')"
-          >
-            x
-          </button>
-          <!-- <modal
-      v-show="isModalVisible"
-      @close="closeModal"
-    /> -->
-        </header>
-      </div>
+            <button class="button" @click="showmodal = false">x</button>
+        </div>
+    </transition>
     </div>
-  </transition>
 </template>
 
 <style>
-  .modal-backdrop {
-    position: fixed;
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+#app{
+  /* position: relative; */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+}
+#btn1{
+    position: absolute;
+    left: 50%;
+    top: 150%;
+    transform: translate(-50%,0%);
+  z-index: 999;
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+background-image: url(https://media.istockphoto.com/vectors/play-button-icon-vector-id1194415465?k=20&m=1194415465&s=612x612&w=0&h=GzotgW8-97uNwXyiPk8OSFv54Gs47VVACKCOQAgqvhY=);
+background-position: center;
+background-size: cover;
+cursor: pointer;
+outline: none;
+border: 1px solid black;
+
+}
+.modal-overlay{
+    background-color: #9999997c;
+    position: absolute;
     top: 0;
     bottom: 0;
     left: 0;
-    right: 0;
-    background-color: rgba(0, 0, 0, 0.3);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    z-index: 99999999;
-  }
+ right: 0;
+ min-height: 900vh;
+ z-index: 99;
+}
+.fade-enter-active,
+.fade-leave-active{
+    transition: opacity .5s;
+}
+.fade-enter,
+.fade-leave-to {
+ opacity: 0;
+}
 
-  .modal {
-    background: #FFFFFF;
-    /* box-shadow: 2px 2px 20px 1px; */
-    background-color: transparent;
-    overflow-x: auto;
-    display: flex;
-    flex-direction: column;
-  }
-
-  .modal-header {
-    padding: 25px;
-    display: flex;
-  }
-
-  .modal-header {
-    position: relative;
-    /* border-bottom: 1px solid #eeeeee; */
-    color: #4AAE9B;
-    justify-content: space-between;
-  }
-
-  .btn-close {
-    position: absolute;
-    top: -3%;
+.modal{
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
     right: 0%;
-    border: none;
+    bottom: 0;
+    width: 100%;
+ max-width: 500px;
+    z-index: 99999999;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.modal .button{
+    position: absolute;
+    top: -5%;
+    outline: none;
+    border: 1px solid white;
+    border-radius: 50%;
     font-size: 25px;
-    padding: 10px;
+    width: 50px;
+    height: 50px;
+    right: -5%;
+    transition: opacity .5s;
+    transition: 0.4s ease-out;
     cursor: pointer;
-    /* font-weight: bold; */
-    color: #ffffff;
-    background: transparent;
-  }
-
-  .btn-green {
-    color: white;
-    background: #4AAE9B;
-    border: 1px solid #4AAE9B;
-    border-radius: 2px;
-  }
-
-  .modal-fade-enter,
-  .modal-fade-leave-to {
-    opacity: 0;
-  }
-
-  .modal-fade-enter-active,
-  .modal-fade-leave-active {
-    transition: opacity .8s ease;
-  }
+}
 </style>
+
+<script>
+export default {
+    name:"dialogue",
+    data(){
+        return{
+            showmodal:false
+        }
+    }
+}
+</script>
+
