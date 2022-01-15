f433d9f27711b6710e1b4fc41f06138b22fe76e8
adarshzpatel@gmail.com <adarshzpatel@gmail.com>
2022-01-15 19:55:11 +0530

    added sample tags in demo

--- examples/tag-input/TagInputDemo.vue
+++ examples/tag-input/TagInputDemo.vue
@@ -1,15 +1,14 @@
 <template>
   <div>   
-    <lv-tag-input label="Default" placeholder="Add a Tag"/>
+    <lv-tag-input label="Default" v-model="sampleTags" placeholder="Add a Tag"/>
     <br>
-    <lv-tag-input label="Rounded"  placeholder="Add a tag" rounded="true"/>
+    <lv-tag-input label="Rounded" v-model="sampleTags"  placeholder="Add a tag" rounded="true"/>
     <br>
-    <lv-tag-input label="Custom tag Component" placeholder="Add a tag">
+    <lv-tag-input label="Custom tag Component" v-model="sampleTags" placeholder="Add a tag">
     <template #tag="{content,deleteTag}">
       <div class="custom-tag">
         {{content}}
          <i class="light-icon-trash delete-icon" @click="deleteTag"></i>
-
       </div>
     </template>
     </lv-tag-input>
@@ -23,19 +22,8 @@ import LvTagInput from 'lightvue/tag-input'
 export default{
   data() {
     return {
-      allOptions: {
-        'floating-label': false,
-        'bottom-bar': false,
-        rounded: false,
-        placeholder: 'default color rgba(0, 0, 0, 0.3)',
-        'help-text': 'You can create your Component',
-        label: 'This is the best demo',
-        'placeholder-color': 'rgba(0, 0, 0, 0.3)',
-        'icon-left': 'light-icon-search',
-        'icon-right': 'light-icon-arrow-up-right-circle',
-      },
-      stringProps: ['label', 'placeholder-color', 'icon-left', 'icon-right', 'help-text', 'placeholder'],
-    };
+     sampleTags : ['Tag 1', 'Tag 2', 'Tag 3'] ,
+    }
   },
   components: {
     DocsCardBest,
@@ -58,8 +46,8 @@ export default{
 }
 
 .delete-icon {
-  padding: 2px;
-  border-radius:3rem;
+  padding: 0.2rem;
+  border-radius: 1rem;
   background:rgb(0, 94, 201) ;
 }
 </style>
