47e64a72c0606be01ec24e80c980b7bcd7628109
adarshzpatel@gmail.com <adarshzpatel@gmail.com>
2022-01-18 18:22:05 +0530

    demo added

---
+++ docs/src/pages/vue-components/animations.vue
@@ -0,0 +1,59 @@
+<template>
+  <docs-page-layout :title="$options.title" :description="$options.description" :status="$options.status">
+    <template #title-right> </template>
+    <animation-best-demo />
+    <docs-card-vue title="Basic" file="animations/AnimationDemo" overflow />
+    <template #api>
+      <getting-started :package-name="$options.packageName" :component-name="$options.componentName" />
+      <docs-card title="APIs" :responsive="false">
+        <docs-all-api :api-data="$options.apiData" />
+      </docs-card>
+    </template>
+  </docs-page-layout>
+</template>
+
+<script>
+import DocsPageLayout from '@/components/docs-card/DocsPageLayout.vue';
+import DocsCardVue from '@/components/docs-card/DocsCardVue.vue';
+import DocsCard from '@/components/docs-card/DocsCard.vue';
+import GettingStarted from '@/components/docs-card/GettingStarted.vue';
+import DocsAllApi from '@/components/docs-card/DocsAllApi.vue';
+import BadgeAPI from 'lightvueDocs/example/badge/BadgeApi';
+import { COMPONENT_STATUS } from '@/static/data/constant.ts';
+import AnimationBestDemo from "lightvueDocs/example/animations/AnimationBestDemo.vue"
+
+export default {
+  head: {
+    title: ' Animations Documentation | LightVue ',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { property: 'og:title', content: 'LightVue' },
+      { property: 'og:image', content: 'https://lightvue.com/icon.png' },
+      {
+        property: 'og:site_name',
+        content: 'Badge Documentation | LightVue',
+      },
+      {
+        property: 'og:description',
+        content: 'Add animations and transitions to your components.',
+      },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+  components: {
+    DocsCardVue,
+    DocsCard,
+    GettingStarted,
+    DocsPageLayout,
+    DocsAllApi,
+    AnimationBestDemo
+},
+  title: 'Animations',
+  description: `Add animations and transitions to your components`,
+  packageName: 'animate',
+  componentName: 'LvAnimate',
+  status: COMPONENT_STATUS.UNDER_DEVELOPMENT,
+  apiData: BadgeAPI,
+};
+</script>
---
+++ examples/animations/AnimationBestDemo.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+      <docs-card-best title="Playground">
+      <div :class="selectedAnimation">
+        <lv-button class="lv-animate__slide-in" label="Animate Me!" size="xl"/>
+      </div>
+      <br>
+      <div>
+        <p>Class Name : {{this.selectedAnimation}} </p>
+      </div>
+      <template #props>
+        <lv-dropdown label="Select Animation" v-model="selectedAnimation" :options="animationList" option-label="label" option-value="value"></lv-dropdown>
+      </template>
+      </docs-card-best>
+    </div>
+</template>
+
+<script>
+import LvButton from "lightvue/button"
+import LvDropdown from "lightvue/dropdown"
+import DocsCardBest from '@/components/docs-card/DocsCardBest';
+export default{
+  name:'AnimationBestDemo',
+  data(){
+    return {
+      selectedAnimation: '',
+      animationList: [
+        {label:'Fade in', value:"lv-animate__fade-in"},
+        {label:'Fade out', value:"lv-animate__fade-out"},
+        {label:'Slide in', value:"lv-animate__slide-in--left"},
+        {label:'Slide out', value:"lv-animate__slide-out--right"},
+        {label:'Zoom in', value:"lv-animate__zoom-in"},
+        {label:'Zoom out', value:"lv-animate__zoom-out"},
+        
+      ]
+    }
+  },
+  components:{
+    LvButton,
+    LvDropdown,
+    DocsCardBest
+  }
+}
+</script>
