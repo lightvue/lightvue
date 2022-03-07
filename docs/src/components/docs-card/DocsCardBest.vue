99f6fd46710983b5c727d655f726860d19204c25
hrushi-thorat <hrushikeshst54@gmail.com>
2022-03-07 10:34:26 +0530

    Playground-v2 init

--- docs/src/components/docs-card/DocsCardBest.vue
+++ docs/src/components/docs-card/DocsCardBest.vue
@@ -2,18 +2,32 @@
   <div class="best__demo__wrapper" :id="computedId">
     <div class="wrapper__header">
       <h5 class="component__header" @click="$router.push({ hash: `#${computedId}` })"># {{ title }}</h5>
-      <i class="light-icon-adjustments-horizontal dy-props__toggle-drawer" @click="showOptions()"></i>
     </div>
     <div class="dy-pg__wrap">
-      <div class="dy-pg__left">
-        <div class="dy-comp__wrap light-scrollbar">
+      <div style="flex: 1" class="dy-pg__left">
+        <div class="dy-comp__wrap light-scrollbar" style="position: relative">
           <slot></slot>
+          <div class="dy-pg_btn-group">
+            <div class="d-flex">
+              <div class="docs-card__action" @click="openURL(sourceLink)">
+                <i class="docs-card__action-icon light-icon-brand-github"></i>
+              </div>
+              <div class="docs-card__action">
+                <i class="docs-card__action-icon light-icon-devices" title="Responsive preview"></i>
+              </div>
+              <div v-if="$slots['code']" class="docs-card__action">
+                <i class="docs-card__action-icon light-icon-code" title="View code example" @click="showCode = !showCode"></i>
+              </div>
+            </div>
+          </div>
         </div>
+
         <div class="dy-code__wrap light-scrollbar">
           <CopyButton :text="getMarkup" />
           <slot name="code"></slot>
         </div>
       </div>
+
       <div class="dy-props__wrap" :class="`${showPorpsOptions ? '--mobile-show' : ''}`">
         <div class="dy-props__header">All Props</div>
         <div class="dy-props__body light-scrollbar">
@@ -26,6 +40,7 @@
 
 <script>
 import CopyButton from '@/components/docs-card/CopyButton';
+
 export default {
   data() {
     return {
@@ -59,14 +74,14 @@ export default {
 <style lang="scss">
 .best__demo__wrapper {
   box-shadow: 0px 0px 2px 1px rgba(0, 0, 0, 0.15);
-  background: #ffffff;
+  background: #edf2f7;
   margin-bottom: 30px;
   width: 100%;
   position: relative;
 }
 .dy-pg__wrap {
   display: flex;
-  justify-content: space-between;
+  /* justify-content: space-between; */
   position: relative;
   /* margin-bottom: ; */
   /* border: 1px solid #cccccc; */
@@ -80,6 +95,7 @@ export default {
   max-height: 60vh;
   align-items: stretch;
   overflow: hidden;
+  position: relative;
 }
 
 .dy-props__wrap {
@@ -114,23 +130,23 @@ export default {
   align-items: center;
   border-bottom: 1px solid #edf2f6;
   .component__header {
-    padding: 20px;
-    color: teal;
+    /* padding: 20px; */
+
     font-size: 18px;
     font-weight: bold;
-    border: 1px solid #edf2f6;
+    /* border: 1px solid #edf2f6; */
     margin: 0px;
     white-space: nowrap;
-    margin-left: -22px;
-    padding: 8px 16px 8px 24px;
-    background: #dfe7eb;
+    /* margin-left: -22px; */
+    /* padding: 8px 16px 8px 24px; */
+    /* background: #dfe7eb; */
     color: #607b89;
     position: relative;
     cursor: pointer;
-    border-radius: 6px 20px 20px 0;
-    box-shadow: 1px 2px 4px -1px rgba(0, 0, 0, 0.4);
+    /* border-radius: 6px 20px 20px 0;
+    box-shadow: 1px 2px 4px -1px rgba(0, 0, 0, 0.4); */
   }
-  h5::after {
+  /* h5::after {
     content: '';
     position: absolute;
     top: 100%;
@@ -142,6 +158,38 @@ export default {
     border-width: 10px 0 0 12px;
     z-index: -1;
     margin: 0px;
+  } */
+}
+.dy-pg_btn-group {
+  position: absolute;
+  bottom: 0%;
+  right: 50%;
+  transform: translate(50%, 50%);
+  width: 10rem;
+  z-index: 1000;
+  padding: 5px 15px;
+  border-radius: 50px;
+  background: #ffffff;
+  /* box-shadow: 0px 20px 50px -10px rgba(0, 0, 0, 0.2); */
+  box-shadow: 0px 25px 80px rgba(0, 0, 0, 0.15);
+  .d-flex {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 24px;
+
+    .docs-card__action-icon {
+      font-weight: bold;
+
+      color: #afafaf;
+
+      cursor: pointer;
+      transition: 0.5s all ease-out;
+    }
+    .docs-card__action-icon:hover {
+      /* color: #38b2ac; */
+      opacity: 0.8;
+    }
   }
 }
 
@@ -149,7 +197,7 @@ export default {
   flex-grow: 1;
   min-height: 200px;
   // display: flex;
-  overflow-y: auto;
+  /* overflow-y: auto; */
   justify-content: center;
   width: 100%;
   padding: 48px;
