983271656b39dd0a74026d3814e5a2b76035fc08
hrushi-thorat <hrushikeshst54@gmail.com>
2022-03-09 13:25:39 +0530

    Added:withoutButton demo

---
+++ examples/number/Number_WithoutButtonDemo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <br />
+    <lv-number placeholder="Enter Number" label="Without Buttons" :controls="false" />
+    <br />
+    <lv-number placeholder="Enter Number" label="Without Buttons" :controls="false" rounded />
+    <br />
+    <lv-number placeholder="Enter Number" label="Without Buttons" :controls="false" bottom-bar />
+    <br />
+  </div>
+</template>
+
+<script>
+import LvNumber from 'lightvue/number';
+
+// import LvNumber from 'lightvue/number';
+export default {
+  components: {
+    LvNumber,
+  },
+};
+</script>
