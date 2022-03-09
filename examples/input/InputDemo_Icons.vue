e75b09ced559e0e2e59d65e89f1af4abf76f2118
hrushi-thorat <hrushikeshst54@gmail.com>
2022-03-09 12:27:53 +0530

    seperate input demo

---
+++ examples/input/InputDemo_Icons.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <lv-input type="text" v-model="value1" icon-left="light-icon-search" icon-right="light-icon-arrow-up-right-circle" label="Left Right Icons" clearable />
+    <br />
+    <lv-input type="text" v-model="value1" icon-left="light-icon-search" icon-right="light-icon-arrow-up-right-circle" label="Left Right Icons" rounded />
+  </div>
+</template>
+
+<script>
+import LvInput from 'lightvue/input';
+export default {
+  data() {
+    return {
+      value1: '',
+    };
+  },
+  components: {
+    LvInput,
+  },
+};
+</script>
+
+<style></style>
---
+++ examples/input/InputDemo_MaterialEffect.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <lv-input id="username" type="text" v-model="value1" label="Material Effect" placeholder="Enter username here..." bottom-bar />
+    <br />
+    <lv-input id="username" type="text" v-model="value1" label="Floating Label" placeholder="Enter username here..." floating-label />
+    <br />
+  </div>
+</template>
+
+<script>
+import LvInput from 'lightvue/input';
+export default {
+  data() {
+    return {
+      value1: '',
+    };
+  },
+  components: {
+    LvInput,
+  },
+};
+</script>
+
+<style></style>
