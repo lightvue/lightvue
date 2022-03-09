dd6f0c7a8bbf09dfd35b59ec0c9c6dc78e7e62d8
hrushi-thorat <hrushikeshst54@gmail.com>
2022-03-09 12:29:00 +0530

    Added:prepend append demo

---
+++ examples/input/InputDemo_PrependAppend.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <lv-input type="text" v-model="value1" placeholder="Search" label="Prepend Append" clearable>
+      <template #prepend>
+        <LvButton icon="light-icon-search" class="lv--secondary" />
+      </template>
+      <template #append>
+        <LvButton icon="light-icon-arrow-up-right-circle" class="lv--danger" />
+      </template>
+    </lv-input>
+    </br>
+        <lv-input type="text" v-model="value1" placeholder="Search" label="Prepend Append" rounded bottom-bar>
+      <template #prepend>
+        <LvButton icon="light-icon-search" class="lv--secondary" />
+      </template>
+      <template #append>
+        <LvButton icon="light-icon-arrow-up-right-circle" class="lv--danger" />
+      </template>
+    </lv-input>
+  </div>
+</template>
+
+<script>
+import LvInput from 'lightvue/input';
+export default {
+  data() {
+    return {
+      value1: '',
+    };
+  },
+  components: {
+    LvInput,
+  },
+};
+</script>
+
+<style></style>
