ceada8855f6af929fbd2576be6fa7a095d2ad31d
hrushi-thorat <hrushikeshst54@gmail.com>
2022-03-10 10:22:27 +0530

    fixed:bestDemo

--- examples/colorpicker/ColorPickerBestDemo.vue
+++ examples/colorpicker/ColorPickerBestDemo.vue
@@ -10,6 +10,7 @@
         <br />
 
         <lv-input v-model="allOptions['label']" label="label" /> <br />
+        <!-- <lv-input v-model="allOptions['modelValue']" label="Modal Value" /> <br /> -->
 
         <br />
       </template>
@@ -30,20 +31,21 @@
 import DocsCardBest from '@/components/docs-card/DocsCardBest';
 
 import LvInput from 'lightvue/input';
-import LvNumber from 'lightvue/number';
+
 import LvColorpicker from 'lightvue/color-picker';
 import LvToggleSwitch from 'lightvue/toggle-switch';
 export default {
   data() {
     return {
       allOptions: {
-        label: 'checkbox',
+        label: 'Choose Color',
+        modelValue: '#38b2ac',
 
         hidePalette: false,
-        withoutInput: true,
+        withoutInput: false,
         colors: ['#F44336', '#E91E63', '#9C27B0', '#673AB7', '#3F51B5', '#2196F3', '#03A9F4', '#00BCD4', '#009688', '#4CAF50', '#8BC34A', '#CDDC39', '#FFEB3B', '#FFC107', '#FF9800', '#795548'],
       },
-      stringProps: ['label'],
+      stringProps: ['label', 'modelValue'],
     };
   },
   components: {
@@ -51,8 +53,6 @@ export default {
     LvInput,
     LvToggleSwitch,
     LvColorpicker,
-    LvCheckBox,
-    LvNumber,
   },
   computed: {
     enabledOptions() {
