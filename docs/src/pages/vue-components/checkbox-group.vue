9d8d48a4b65c96c07959a5762c062b5c1e76efdc
hrushi-thorat <hrushikeshst54@gmail.com>
2022-03-10 11:58:38 +0530

    update :checkbox Demo File+added description

--- docs/src/pages/vue-components/checkbox-group.vue
+++ docs/src/pages/vue-components/checkbox-group.vue
@@ -1,7 +1,10 @@
 <template>
   <docs-page-layout :title="$options.title" :description="$options.description">
-    <docs-card-vue title="Basic" file="checkbox-group/CheckboxGroupDemo_Basic" />
+    <!-- <docs-card-vue title="Basic" file="checkbox-group/CheckboxGroupDemo_Basic" /> -->
     <!-- <docs-card-vue title="Basic" file="checkbox/CheckboxDemo" /> -->
+    <template v-for="(demo, i) in demos">
+      <docs-card-vue :title="demo.title" :discription="demo.discription" :file="demo.file" :id="demo.id" />
+    </template>
     <template #api>
       <getting-started :package-name="$options.packageName" :component-name="$options.componentName" />
       <docs-all-api :api-data="$options.apiData" />
@@ -35,6 +38,32 @@ export default {
       },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+  data() {
+    return {
+      demos: [
+        {
+          title: 'Basic',
+          file: 'checkbox-group/CheckboxGroupDemo_Basic',
+          discription: 'You can pass the <span>options</span> in <span>lv-checkbox-group</span> as an array of objects. You can still use all the props and variations in the <span>LvCheckbox</span>  .',
+        },
+        {
+          title: 'Set option value',
+          file: 'checkbox-group/CheckboxGroupDemo1',
+          discription: 'You can set the option value and label through <sapn>optionValue</sapn> and <span>optionLabel</span> respectively',
+        },
+        {
+          title: 'Array of strings',
+          discription: 'You can also use array of strings for options',
+          file: 'checkbox-group/CheckboxGroupDemo2',
+        },
+        {
+          title: 'Preselect Value',
+          discription: 'You can set the value selected by default by setting it as an <span>v-model</span> value',
+          file: 'checkbox-group/CheckboxGroupDemo3',
+        },
+      ],
+    };
   },
   components: {
     DocsCardVue,
