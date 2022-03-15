85f773f63a16fa226e0f2913ed3395f3a7f1a6d4
hrushi-thorat <hrushikeshst54@gmail.com>
2022-03-15 11:36:08 +0530

    Fixed:collapsible demo

--- examples/collapsible/AccordianDemo.vue
+++ examples/collapsible/AccordianDemo.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="accordian-wrapper">
     <div class="nav-list__category-wrap">
       <div class="collapsible_catgory" @click="open1">
         <div class="nav-list__category-label">
@@ -10,7 +10,7 @@
       </div>
       <LvCollapsible :show="isOpenA">
         <div class="collapisible--answer">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam quasi ab blanditiis, iure consectetur, eius rerum vitae ipsa doloremque beatae sapiente? Alias ea atque sint excepturi. Possimus consequatur distinctio sequi.</p>
+          <p>LightVue is the most comprehensive, performant and easy to use set of UI components for Vue.js. It provides a wide range of high quality Widgets and Plugins built with accessibility, responsiveness and performance in mind.</p>
         </div>
       </LvCollapsible>
     </div>
@@ -38,7 +38,7 @@
       </div>
       <LvCollapsible :show="isOpenC">
         <div class="collapisible--answer">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam quasi ab blanditiis, iure consectetur, eius rerum vitae ipsa doloremque beatae sapiente? Alias ea atque sint excepturi. Possimus consequatur distinctio sequi.</p>
+          <p>lightVue is an open source UI component library for Vue 2.x and 3.x projects. It's designed to be a readily available resource of well-designed, accessible, and responsive components for your future projects.</p>
         </div>
       </LvCollapsible>
     </div>
@@ -78,7 +78,7 @@ export default {
 };
 </script>
 <style lang="scss">
-.lv-ink {
-  background-color: #38b2ac;
+.accordian-wrapper {
+  width: 100%;
 }
 </style>
