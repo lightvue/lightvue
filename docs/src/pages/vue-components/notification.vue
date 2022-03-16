88542eb8a7e5cda50cb11ecd69f1aa13aac8b36f
hrushi-thorat <hrushikeshst54@gmail.com>
2022-03-16 08:48:36 +0530

    Update:notification docs

--- docs/src/pages/vue-components/notification.vue
+++ docs/src/pages/vue-components/notification.vue
@@ -1,11 +1,12 @@
 <template>
   <docs-page-layout :title="$options.title" :description="$options.description" :status="$options.status">
-    <docs-card-vue title="Basic" file="notification/NotificationDemo" overflow />
+    <template v-for="(demo, i) in demos">
+      <docs-card-vue :title="demo.title" :discription="demo.discription" :file="demo.file" :id="demo.id" />
+    </template>
     <template #api>
       <getting-started :package-name="$options.packageName" :component-name="$options.componentName" />
-      <docs-card title="APIs" :responsive="false">
-        <docs-all-api :api-data="$options.apiData" />
-      </docs-card>
+
+      <docs-all-api :api-data="$options.apiData" />
     </template>
   </docs-page-layout>
 </template>
@@ -36,6 +37,35 @@ export default {
       },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+  data() {
+    return {
+      demos: [
+        {
+          title: 'Variants',
+          file: 'notification/NotificationDemo',
+          discription: 'Use the buttons with some basic and complex use cases by passing either classes or props eg. for link button, pass <span>--link-button</span> For disable button, set the <span>disable</span> prop to true.',
+        },
+        {
+          title: 'Positions',
+          file: 'notification/NotificationDemo_Position',
+          discription: 'Use the buttons with some basic and complex use cases by passing either classes or props eg. for link button, pass <span>--link-button</span> For disable button, set the <span>disable</span> prop to true.',
+          // discription: 'You can use the buttons with some basic use cases by simply passing classes or by passing props eg.for link button you can pass <span>--link-button</span> for disable button set the <span>disable</span> prop to true',
+        },
+        {
+          title: 'Options',
+          file: 'notification/NotificationDemo_Option',
+          discription: 'Use the buttons with some basic and complex use cases by passing either classes or props eg. for link button, pass <span>--link-button</span> For disable button, set the <span>disable</span> prop to true.',
+          // discription: 'You can use the buttons with some basic use cases by simply passing classes or by passing props eg.for link button you can pass <span>--link-button</span> for disable button set the <span>disable</span> prop to true',
+        },
+        {
+          title: 'Remove All',
+          file: 'notification/NotificationDemo_RemoveAll',
+          discription: 'Use the buttons with some basic and complex use cases by passing either classes or props eg. for link button, pass <span>--link-button</span> For disable button, set the <span>disable</span> prop to true.',
+          // discription: 'You can use the buttons with some basic use cases by simply passing classes or by passing props eg.for link button you can pass <span>--link-button</span> for disable button set the <span>disable</span> prop to true',
+        },
+      ],
+    };
   },
   components: {
     DocsCardVue,
