6fff0a3103a2e8348397dbb55fb9ade6a7dcecf9
hrushi-thorat <hrushikeshst54@gmail.com>
2022-03-16 08:49:54 +0530

    Seperate:toast Demo

---
+++ examples/toast/ToastDemo_Position.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <LvButton label="Top" class="--mr-4" @click="showTop" />
+    <LvButton label="Center" class="--mr-4" @click="showCenter" />
+    <LvButton label="Bottom" class="lv--warning --mr-4" @click="showBottom" />
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    showTop() {
+      this.$toast.add({ type: 'info', title: 'Info Message', content: 'Message Content', position: 'top', duration: 3000 });
+    },
+    showCenter() {
+      this.$toast.add({ type: 'info', title: 'Info Message', content: 'Message Content', position: 'center', duration: 3000 });
+    },
+    showBottom() {
+      this.$toast.add({ type: 'warn', title: 'Warn Message', content: 'Message Content', position: 'bottom', duration: 3000 });
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.--mr-4 {
+  margin-right: 8px;
+}
+</style>
