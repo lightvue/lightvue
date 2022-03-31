9f7d13115a7dfa916111ba12095d883689e95a34
hrushi-thorat <hrushikeshst54@gmail.com>
2022-03-31 11:31:05 +0530

    Fixed : typo

--- docs/src/pages/vue-components/textarea.vue
+++ docs/src/pages/vue-components/textarea.vue
@@ -47,7 +47,6 @@ export default {
           title: 'Basic',
           file: 'textarea/TextareaDemo__Basic',
           discription: 'You can use the <span>LvTextarea</span> with some basic and complex use cases by passing either classes or props eg. If you want to set the placeholder you can pass it to <span>placeholder</span>, You can also set the number of visible rows through <span>rows</span> prop, You can set the Label by passing a string into <span>label</span> prop.LightVue textarea is Both compatible with Vue 2.x and Vue 3.x',
-        
         },
 
         {
@@ -61,7 +60,7 @@ export default {
           file: 'textarea/TextAreaAutoResizeDemo',
         },
         {
-          title: 'Vertival Resize',
+          title: 'Vertical Resize',
           discription: 'You can define the behaviour of <span>LvTextarea</span> after characters exceed text area height either it will scroll or get resize the scrollable behaviour is default but if you want to auto resize the text area vertically ,You have to set <span>resize</span> prop to <span>vertical</span>',
           file: 'textarea/TextAreaVerticalResize',
         },
