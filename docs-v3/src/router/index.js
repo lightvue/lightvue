b88983addd0cef5936e2f572abbf70a1ab6a0b76
Pulkit <pulkit.cloud@gmail.com>
2022-04-09 04:57:30 +0530

    Centralized logic for defaultScrollBehaviour

--- docs-v3/src/router/index.js
+++ docs-v3/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 // import Sample from '../components/examples/Sample.vue';
+import defaultScrollBehaviour from '@/app/router.scrollBehavior.js';
 
 const routes = [
   {
@@ -156,23 +157,7 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
-  scrollBehavior: (to, from, savedPosition) => {
-    if (to.fullPath === from.fullPath) {
-      // in case of preventBrowserBackMixin
-      return;
-    }
-    if (to.hash) {
-      setTimeout(() => {
-        const el = document.getElementById(to.hash.substring(1));
-        if (el) {
-          // a valid id on page.
-          return window.scrollTo({ top: el.offsetTop - 20, behavior: 'smooth' });
-        }
-      }, 300); // wait before scrolling, for initial loading delay
-      return;
-    }
-    return window.scrollTo({ top: 0, behavior: 'smooth' });
-  },
+  scrollBehavior: defaultScrollBehaviour,
 });
 
 export default router;
