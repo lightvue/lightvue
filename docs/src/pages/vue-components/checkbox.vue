bb0c328c9589e0ee45c4a57a635186ffc58ad051
Hrushikesh Thorat <hrushikeshst54@gmail.com>
2022-04-17 09:42:01 +0530

    Update: checkbox demo render + Update title

--- docs/src/pages/vue-components/checkbox.vue
+++ docs/src/pages/vue-components/checkbox.vue
@@ -1,13 +1,11 @@
 <template>
-  <docs-page-layout :title="$options.title" :description="$options.description" :cards="demos">
+  <docs-page-layout :title="$options.title" :description="$options.description" :demoList="$options.demoList" hasPlayground hasDocs>
     <checkbox-best-demo></checkbox-best-demo>
-    <template v-for="demo in demos">
-      <docs-card-vue :title="demo.title" :discription="demo.discription" :file="demo.file" :id="demo.id" />
-    </template>
     <template #api>
       <getting-started :package-name="$options.packageName" :component-name="$options.componentName" />
       <docs-all-api :api-data="$options.apiData" />
     </template>
+    <docs-card-vue v-for="demo in $options.demoList" :key="demo.id" :title="demo.title" :discription="demo.discription" :file="demo.file" :id="demo.id" />
   </docs-page-layout>
 </template>
 
@@ -47,47 +45,49 @@ export default {
     DocsAllApi,
     CheckboxBestDemo,
   },
-  data() {
-    return {
-      demos: [
-        {
-          title: 'Basic',
-          file: 'checkbox/CheckboxDemo_Basic',
-          discription: 'You can use the checkbox with various predefined colors by passing them in <span>color</span> prop, avaliable colors are <span>sucess</span>,<span>warning</span>,<span>primary</span>,<span>info</span>,<span>danger</span>. You can set the corners of checkbox to round by setting <span>rounded</span> prop to true.you can change the appearence of checkbox through <span>thick</span>,<span>filled</span>,<span>plain</span>,<span>smooth</span>.LightVue checkbox is Both compatible with Vue 2.x and Vue 3.x',
-        },
-        {
-          title: 'With Animations',
-          file: 'checkbox/CheckboxDemo_Animation',
-          discription: 'You can use the checkbox with various predefined  Transitions by setting <span>smooth</span>,<span>pulse</span> props to true',
-        },
-        {
-          title: 'Radio',
-          file: 'checkbox/CheckboxDemo_Radio',
-          discription: 'You can use the checkbox component as a <span>Radio</span> by setting <span>type</span> prop to <span>radio</span> all the other variations you use with the checkbox you can use them with radio also in the same way ',
-        },
-
-        {
-          title: 'Disabled',
-          file: 'checkbox/CheckboxDemo_Disabled',
-          discription: 'You can use the checkbox to Disable by passing <span>disabled</span> prop to true',
-        },
-        {
-          title: 'With Icons',
-          file: 'checkbox/CheckboxDemo_Icons',
-          discription: 'You can use the icons with checkbox by passing then in the slot and setting <span>icon</span> prop to true',
-        },
-        {
-          title: 'Scalable sizes',
-          file: 'checkbox/CheckboxDemo_Size',
-          discription: 'You can set the checkbox size by wrapping it inside a element',
-        },
-      ],
-    };
-  },
   title: 'Checkbox',
   description: `LvCheckbox component can be used in projects where a way is required for user to select or deselect an option.`,
   packageName: 'CheckBox',
   componentName: 'LvCheckBox',
   apiData: CheckboxAPI,
+  demoList: [
+    {
+      id: 'basic-checkbox__demo',
+      title: 'Basic',
+      file: 'checkbox/CheckboxDemo_Basic',
+      discription: 'You can use the checkbox with various predefined colors by passing them in <span>color</span> prop, avaliable colors are <span>sucess</span>,<span>warning</span>,<span>primary</span>,<span>info</span>,<span>danger</span>. You can set the corners of checkbox to round by setting <span>rounded</span> prop to true.you can change the appearence of checkbox through <span>thick</span>,<span>filled</span>,<span>plain</span>,<span>smooth</span>.LightVue checkbox is Both compatible with Vue 2.x and Vue 3.x',
+    },
+    {
+      id: 'with-animations',
+      title: 'With animations',
+      file: 'checkbox/CheckboxDemo_Animation',
+      discription: 'You can use the checkbox with various predefined  Transitions by setting <span>smooth</span>,<span>pulse</span> props to true',
+    },
+    {
+      id: 'radio-demo',
+      title: 'Radio',
+      file: 'checkbox/CheckboxDemo_Radio',
+      discription: 'You can use the checkbox component as a <span>Radio</span> by setting <span>type</span> prop to <span>radio</span> all the other variations you use with the checkbox you can use them with radio also in the same way ',
+    },
+
+    {
+      id: 'disable-checkbox',
+      title: 'Disabled',
+      file: 'checkbox/CheckboxDemo_Disabled',
+      discription: 'You can use the checkbox to Disable by passing <span>disabled</span> prop to true',
+    },
+    {
+      id: 'with-icons',
+      title: 'With icons',
+      file: 'checkbox/CheckboxDemo_Icons',
+      discription: 'You can use the icons with checkbox by passing then in the slot and setting <span>icon</span> prop to true',
+    },
+    {
+      id: 'scalable-sizes',
+      title: 'Scalable sizes',
+      file: 'checkbox/CheckboxDemo_Size',
+      discription: 'You can set the checkbox size by wrapping it inside a element',
+    },
+  ],
 };
 </script>
