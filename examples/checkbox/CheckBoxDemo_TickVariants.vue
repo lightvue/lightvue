6754ac341a474181f8ddee5d4bd1cc891439521b
Ekta Chaudhary <60789158+itsekta@users.noreply.github.com>
2022-08-24 20:26:48 +0530

    Fix: Centered the Checkbox Tick Variant

--- examples/checkbox/CheckBoxDemo_TickVariants.vue
+++ examples/checkbox/CheckBoxDemo_TickVariants.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="lv-demo_layout">
     <lv-checkbox name="check" color="success" v-model="checked" checkedIcon outline thick >Basic</lv-checkbox>
-    <lv-checkbox name="check" type="radio" color="warning" v-model="radio" thick checkedIcon>Orange</lv-checkbox>
-    <lv-checkbox name="check" type="radio" color="primary" v-model="radio" thick outline checkedIcon>Blue </lv-checkbox>
-    <lv-checkbox name="check" color="info" v-model="checked" checkedIcon rounded thick>Light Blue </lv-checkbox>
+    <lv-checkbox name="check" color="warning" v-model="checked" thick checkedIcon>Orange</lv-checkbox>
+    <lv-checkbox name="check" color="primary" v-model="checked" thick outline checkedIcon>Blue </lv-checkbox>
+    <lv-checkbox name="check" color="info" v-model="checked" checkedIcon  rounded thick pulse>Light Blue </lv-checkbox>
     <lv-checkbox name="check" color="danger" v-model="checked" rounded checkedIcon outline thick> Red </lv-checkbox>
   </div>
 </template>
@@ -15,20 +15,20 @@ export default {
   data() {
     return {
       checked: false,
-      radio: '',
-      cities: [],
-      categories: [
-        { name: 'Accounting', key: 'A' },
-        { name: 'Marketing', key: 'M' },
-        { name: 'Production', key: 'P' },
-        { name: 'Research', key: 'R' },
-      ],
-      selectedCategories: [],
+      // radio: '',
+      // cities: [],
+      // categories: [
+      //   { name: 'Accounting', key: 'A' },
+      //   { name: 'Marketing', key: 'M' },
+      //   { name: 'Production', key: 'P' },
+      //   { name: 'Research', key: 'R' },
+      // ],
+      // selectedCategories: [],
     };
   },
-  created() {
-    this.selectedCategories = this.categories.slice(1, 3);
-  },
+  // created() {
+  //   this.selectedCategories = this.categories.slice(1, 3);
+  // },
   components: {
     LvCheckbox,
   },
