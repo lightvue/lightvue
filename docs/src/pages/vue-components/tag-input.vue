27e279d4dbc0697c8905d4340471e1890c9a5009
Hrushikesh Thorat <hrushikeshst54@gmail.com>
2022-10-02 15:23:24 +0530

    Update: documentation

--- docs/src/pages/vue-components/tag-input.vue
+++ docs/src/pages/vue-components/tag-input.vue
@@ -1,24 +1,20 @@
 <template>
-  <docs-page-layout :title="$options.title" :description="$options.description" :status="$options.status">
-    <template #title-right> </template>
-    <tag-input-best-demo/>
-    <docs-card-vue title="Basic" file="tag-input/TagInputDemo" overflow />
+  <docs-page-layout :title="$options.title" :description="$options.description" :demoList="$options.demoList" hasPlayground hasDocs>
+    <tag-input-best-demo />
+    <docs-card-vue v-for="demo in $options.demoList" :key="demo.id" :title="demo.title" :description="demo.description" :file="demo.file" :id="demo.id" />
     <template #api>
       <getting-started :package-name="$options.packageName" :component-name="$options.componentName" />
-      <docs-card title="APIs" :responsive="false">
-        <docs-all-api :api-data="$options.apiData" />
-      </docs-card>
+      <docs-all-api :api-data="$options.apiData" />
     </template>
   </docs-page-layout>
 </template>
-
 <script>
 import DocsPageLayout from '@/components/docs-card/DocsPageLayout.vue';
 import DocsCardVue from '@/components/docs-card/DocsCardVue.vue';
 import DocsCard from '@/components/docs-card/DocsCard.vue';
 import GettingStarted from '@/components/docs-card/GettingStarted.vue';
 import DocsAllApi from '@/components/docs-card/DocsAllApi.vue';
-import BadgeAPI from 'lightvueDocs/example/badge/BadgeApi';
+import TagInputAPI from 'lightvueDocs/example/tag-input/TagInputAPI';
 import TagInputBestDemo from 'lightvueDocs/example/tag-input/TagInputBestDemo';
 import { COMPONENT_STATUS } from '@/static/data/constant.ts';
 
@@ -32,11 +28,11 @@ export default {
       { property: 'og:image', content: 'https://lightvue.com/icon.png' },
       {
         property: 'og:site_name',
-        content: 'Badge Documentation | LightVue',
+        content: 'TagInput Documentation | LightVue',
       },
       {
         property: 'og:description',
-        content: 'Badge component superscripts or subscripts an avatar-like icon or text onto content to highlight information to a user or to just draw attention to a specific element. Content within the badge usually contains numbers or icons.',
+        content: 'The Lightvue Tag Input component allow user to type in multiple values as tags.',
       },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
@@ -47,13 +43,45 @@ export default {
     GettingStarted,
     DocsPageLayout,
     DocsAllApi,
-    TagInputBestDemo
+    TagInputBestDemo,
   },
   title: 'Tag Input',
-  description: `Take array of inputs  `,
+  description: `The Lightvue Tag Input component allow user to type in multiple values as tags.`,
   packageName: 'tag-input',
   componentName: 'LvTagInput',
   status: COMPONENT_STATUS.UNDER_DEVELOPMENT,
-  apiData: BadgeAPI,
+  apiData: TagInputAPI,
+  demoList: [
+    {
+      id: 'Default',
+      title: 'Default',
+      file: 'tag-input/TagInputDemo',
+      description: 'Lightvue comes with stunnig look and feel for <span>LvTagInput</span> component. This is the default usecase for LvTagInput component.LvTagInput is both compatible with <span>Vue 2</span> and <span>Vue 3</span>',
+    },
+    {
+      id: 'rounded-tag-input',
+      title: 'Rounded',
+      file: 'tag-input/TagInputRounded',
+      description: 'You can use the rounded version of tag input by setting <span>Rounded</span> prop true',
+    },
+    {
+      id: 'custom-tag-input',
+      title: 'Custom',
+      description: '<span>LvTagInput</span> is totally customizable vue component. You can change the look and feel of tag input in the following way',
+      file: 'tag-input/TagInputCustomTagDemo',
+    },
+    {
+      id: 'object-as-input',
+      title: 'Object Datatype',
+      description: 'You can use object as a v-model value for LvTagInput',
+      file: 'tag-input/TagInputObjectDemo',
+    },
+    {
+      id: 'Limit',
+      title: 'Limit Tags',
+      description: 'limit tags using <span>limit</span> prop which takes number as input LvTagInput',
+      file: 'tag-input/TagInputLimitDemo',
+    },
+  ],
 };
 </script>
