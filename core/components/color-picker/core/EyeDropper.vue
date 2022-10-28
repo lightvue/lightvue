b01d1bef501886d7c084514920e9cfeaf98f04b9
jay-p-b-7span <jay.p.b@7span.com>
2022-10-28 16:55:57 +0530

    Browser Compatibility Check done

--- core/components/color-picker/core/EyeDropper.vue
+++ core/components/color-picker/core/EyeDropper.vue
@@ -17,14 +17,17 @@ export default {
 
   methods: {
     async start() {
-      const instance = new EyeDropper();
-      try {
-        const result = await instance.open();
-        const hexFormat = tinycolor(result.sRGBHex).toHexString();
-        this.$emit('color-selected', hexFormat);
-      } catch {
-        // This catch block is executed when promise gets rejected when user presses escape button instead of selecting any pixel
-      }
+      if (window.EyeDropper) {
+        // Check for Browser Compatibility
+        const instance = new EyeDropper();
+        try {
+          const result = await instance.open();
+          const hexFormat = tinycolor(result.sRGBHex).toHexString();
+          this.$emit('color-selected', hexFormat);
+        } catch {
+          // This catch block is executed when promise gets rejected when user presses escape button instead of selecting any pixel
+        }
+      } else this.$toast.add({ type: 'error', title: 'Browser InCompatible', content: 'Your Browser does not support EyeDropper. Please use Google Chrome.', duration: 3000 });
     },
   },
 };
