67cb152b4cf7f03eef65a66d8c7f8bbde25312bc
Pulkit <pulkit.cloud@gmail.com>
2023-02-22 19:12:09 +0530

    Fixed: landing-page dark mode

--- docs/src/layouts/landing.vue
+++ docs/src/layouts/landing.vue
@@ -14,20 +14,4 @@ export default {};
 
 <style>
 @import './overwrite.scss';
-* {
-  padding: 0px;
-  margin: 0px;
-  box-sizing: border-box;
-}
-a {
-  text-decoration: none;
-}
-
-body,
-#app {
-  background-color: #f5f8fa;
-  font-family: Roboto, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
 </style>
